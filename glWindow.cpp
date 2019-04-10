b5357c192fd2ad8e23e6b144d440c75cd8d1099e
SilasHenderson <46079402+SilasHenderson@users.noreply.github.com>
2019-04-10 09:24:53 -0400

    Add files via upload

---
+++ glWindow.cpp
@@ -0,0 +1,6 @@
+#include <GL/glut.h> 						// gl Utility toolbox
+
+int main(int argc, char **argv) {
+	glutInit(&argc, argv); 					// Initialize gl utilities
+	glutCreateWindow("bananas");         	// Make window
+	glutMainLoop();}                   		// Run loop
