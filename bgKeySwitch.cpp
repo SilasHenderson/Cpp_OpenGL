d59a4eee099f6433361e2b9c78cccd6ea97f2ae3
SilasHenderson <46079402+SilasHenderson@users.noreply.github.com>
2019-04-11 13:37:16 -0400

    Update bgKeySwitch.cpp

--- bgKeySwitch.cpp
+++ bgKeySwitch.cpp
@@ -1,24 +1,23 @@
 // cppGL color switch {Silas Henderson 2019} 
 // -- up/down key change window color
-#include <GL/glut.h> 										// gl Utility toolbox
-
+#include <GL/glut.h> 										
 float red = .1; 		
 
-void keyboard(int key, int x, int y) {   					// Keyboard        
+void keyboard(int key, int x, int y) {   					        
 	switch (key) {
 		case GLUT_KEY_UP:    red = .5; glutPostRedisplay(); break;
 		case GLUT_KEY_DOWN:  red = .1; glutPostRedisplay(); break;}}
 
-void display() { 											// Display
-	glClearColor(red, .1, .1, 1);							// -- color
-	glClear(GL_COLOR_BUFFER_BIT);  							// -- clear
-	glutSwapBuffers();} 									// -- update
+void display() { 											
+	glClearColor(red, .1, .1, 1);						
+	glClear(GL_COLOR_BUFFER_BIT);  						
+	glutSwapBuffers();} 								
 
-int main(int argc, char **argv) { 							// Main
-	glutInit(&argc, argv); 									// -- initialize ut
-	glutInitDisplayMode(GLUT_RGB| GLUT_DOUBLE); 			// -- display props
-	glutCreateWindow("bananas");                        	// -- open window
+int main(int argc, char **argv) { 							
+	glutInit(&argc, argv); 								
+	glutInitDisplayMode(GLUT_RGB| GLUT_DOUBLE); 			
+	glutCreateWindow("bananas");                        	
 
-	glutDisplayFunc(display); 								// -- display Fcn
-	glutSpecialFunc(keyboard);                      		// -- keyboard Fcn
-	glutMainLoop();}                   						// -- main Fcn
+	glutDisplayFunc(display); 								
+	glutSpecialFunc(keyboard);                      		
+	glutMainLoop();}                   						
