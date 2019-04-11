57f2cff30301821f9ea148e4fce6824127894d36
SilasHenderson <46079402+SilasHenderson@users.noreply.github.com>
2019-04-11 13:42:09 -0400

    Update cppBreakOut.cpp

--- cppBreakOut.cpp
+++ cppBreakOut.cpp
@@ -1,59 +1,55 @@
 // break-out {Silas Henderson 2019}
 
-#include <GL/freeglut.h>                                                    // libraries
+#include <GL/freeglut.h>                                                   
 #include <math.h>                                                         
 
-int programOn = 1;                                                          // program switch
+int programOn  = 1;     float boxPos   =  0;                                                         
+int leftKey    = 0;     float ballPosX =  3;    float ballVelX = .02;                                                           
+int rightKey   = 0;     float ballPosY = .8;    float ballVelY = .02;
 
-int leftKey  = 0;                                                           // key flags
-int rightKey = 0;
+void update() {                                                             
+    glClear(GL_COLOR_BUFFER_BIT);                                         
 
-float boxPos   =   0;                                                       // box pos
-float ballPosX =  .3;    float ballVelX = .02;
-float ballPosY =  .8;    float ballVelY = .02;
-
-void update() {                                                             // Update Function
-    glClear(GL_COLOR_BUFFER_BIT);                                           // -- clear
-
-    if (ballPosX <  -1 || ballPosX > 1) {ballVelX = -ballVelX;}             // -- bounce
+    if (ballPosX <  -1 || ballPosX > 1) {ballVelX = -ballVelX;}           
     if (ballPosY < -.8 || ballPosY > 1) {ballVelY = -ballVelY;}             
 
-    if (ballPosY < -.8 && abs(ballPosX - boxPos) > .2) {programOn = 0;}     // -- close if miss         
+    if (ballPosY < -.8 && abs(ballPosX - boxPos) > .2) {programOn = 0;}       
     
-    ballPosX = ballPosX + ballVelX;                                         // -- update pos
+    ballPosX = ballPosX + ballVelX;                                     
     ballPosY = ballPosY + ballVelY;
 
-    if (leftKey  == 1) {boxPos = boxPos - .05;}                             // -- move box
+    if (leftKey  == 1) {boxPos = boxPos - .05;}                      
     if (rightKey == 1) {boxPos = boxPos + .05;}
 
-    glRectf(boxPos - .2, -.9, boxPos + .2, -.8);                            // -- draw box
-    glBegin(GL_POINTS); glVertex2f(ballPosX, ballPosY); glEnd();            // -- draw ball
+    glRectf(boxPos - .2, -.9, boxPos + .2, -.8);  
+    glBegin(GL_POINTS); 
+        glVertex2f(ballPosX, ballPosY); 
+    glEnd(); 
     
-    glutSwapBuffers();                                                      // -- refresh
-
-    if (programOn == 0) { glutDestroyWindow(1);  return;}}                  // -- if(close)
+    glutSwapBuffers();                                             
+    if (programOn == 0) { glutDestroyWindow(1);  return;}}    
 
 // ------------------------------ Keyboard ----------------------------
-void keyDown(int key, int x, int y) {                                       // Key Down
+void keyDown(int key, int x, int y) {                                     
     switch (key) {  case GLUT_KEY_LEFT:    leftKey = 1; break;
                     case GLUT_KEY_RIGHT:  rightKey = 1; break; }}
 
-void keyUp(  int key, int x, int y) {                                       // Key Up
+void keyUp(  int key, int x, int y) {                                   
     switch(key) {   case GLUT_KEY_LEFT:    leftKey = 0; break; 
                     case GLUT_KEY_RIGHT:  rightKey = 0; break; }}
 
 // --------------------------- Timer, Main ----------------------------
-void timer(int value) {update(); glutTimerFunc(10, timer, 0);}              // Timer
+void timer(int value) {update(); glutTimerFunc(10, timer, 0);}            
 
-int main(int argc, char** argv) {                                           // Main
-    glutInit(&argc, argv);                                                  // -- init/window
+int main(int argc, char** argv) {                                         
+    glutInit(&argc, argv);                                             
     glutInitDisplayMode(GLUT_DOUBLE|GLUT_RGBA);
     glutCreateWindow("crowTRobot");
     
-    glClearColor(.1, .1, .1, 1);                                            // -- graphics
+    glClearColor(.1, .1, .1, 1);                                
     glPointSize(10);                               
 
-    glutDisplayFunc(update);                                                // -- functions
+    glutDisplayFunc(update);                                      
     glutSpecialFunc(keyDown);
     glutSpecialUpFunc(keyUp);
     glutTimerFunc(40, timer, 0);
